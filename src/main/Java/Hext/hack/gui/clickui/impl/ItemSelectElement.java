7b3a756963db7d1de464c3dbf6c46a11bc949711
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-09 00:50:22 +0300

    Add files via upload

---
+++ src/main/Java/Hext/hack/gui/clickui/impl/ItemSelectElement.java
@@ -0,0 +1,45 @@
+package hext.gui.clickui.impl;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.util.math.MatrixStack;
+import hext.core.Managers;
+import hext.gui.clickui.AbstractElement;
+import hext.gui.font.FontRenderers;
+import hext.gui.windows.WindowsScreen;
+import hext.gui.windows.impl.ItemSelectWindow;
+import hext.setting.Setting;
+import hext.setting.impl.ItemSelectSetting;
+
+import java.awt.*;
+
+import static hext.core.manager.IManager.mc;
+
+public class ItemSelectElement extends AbstractElement {
+    private final Setting<ItemSelectSetting> setting;
+
+    public ItemSelectElement(Setting setting) {
+        super(setting);
+        this.setting = setting;
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        MatrixStack matrixStack = context.getMatrices();
+        FontRenderers.icons.drawString(matrixStack, "H", x + width - 14f, y + 6f, new Color(0xFFECECEC, true).getRGB());
+        FontRenderers.sf_medium_mini.drawString(matrixStack, setting.getName(), x + 6f, (y + height / 2 - 1f), new Color(-1).getRGB());
+    }
+
+    @Override
+    public void mouseClicked(int mouseX, int mouseY, int button) {
+        if (hovered) {
+            mc.setScreen(new WindowsScreen(new ItemSelectWindow(getItemSetting())));
+            Managers.SOUND.playSwipeIn();
+        }
+        super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    public Setting<ItemSelectSetting> getItemSetting() {
+        return setting;
+    }
+}
