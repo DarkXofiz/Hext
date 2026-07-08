18c60068c40edea0c277a5877136c8bba14b970f
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-08 23:32:04 +0300

    Add new features to HudState class

--- src/main/java/com/example/cheat/HudState.java
+++ src/main/java/com/example/cheat/HudState.java
@@ -4,4 +4,8 @@ public class HudState {
     public static boolean showCoords = false;
     public static boolean flyEnabled = false;
     public static boolean zoomEnabled = false;
+    public static boolean HitboxEnabled = false;
+    public static boolean killauraEnabled = false;
+    public static boolean EspEnabled = false;
+    public static double flySpeed = 0.05; // varsayılan vanilla fly hızı
 }
