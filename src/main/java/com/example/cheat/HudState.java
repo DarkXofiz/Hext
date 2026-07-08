1e370067256049f8d1346f20e4428743dd6266d0
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-08 21:54:33 +0300

    Add HudState class with initial boolean fields

--- src/main/java/com/example/cheat/HudState.java
+++ src/main/java/com/example/cheat/HudState.java
@@ -1,1 +1,7 @@
+package com.example.cheat;
 
+public class HudState {
+    public static boolean showCoords = false;
+    public static boolean flyEnabled = false;
+    public static boolean zoomEnabled = false;
+}
