9829403704d19d64e4b38e894918a82f39b6f076
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-09 01:39:27 +0300

    Delete src/main/java /com/hext/mixin directory

--- src/main/java /com/hext/mixin/PlayerEntityMixin.java
+++
@@ -1,9 +0,0 @@
-package com.darkclient.mixin;
-
-import net.minecraft.entity.player.PlayerEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(PlayerEntity.class)
-public class PlayerEntityMixin {
-    // Reserved for player-specific mixins (e.g., food level overrides)
-}
--- src/main/java /com/hext/mixin/WorldRendererMixin.java
+++
@@ -1,9 +0,0 @@
-package com.darkclient.mixin;
-
-import net.minecraft.client.render.WorldRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(WorldRenderer.class)
-public class WorldRendererMixin {
-    // Reserved for world-level render hooks (e.g., custom sky color, ESP outlines)
-}
