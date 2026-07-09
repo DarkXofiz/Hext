d38f55543a03817acadccd1ef68a9ae43c6503e9
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 00:59:21 +0300

    Add files via upload

---
+++ src/main/java /com/hext/client/module/impl /misc/Fullbright.java
@@ -0,0 +1,43 @@
+package com.hextclient.client.module.impl.misc;
+
+import com.hextclient.client.module.Module;
+import com.hextclient.client.module.category.Category;
+import com.hextclient.client.module.setting.SliderSetting;
+import net.minecraft.client.MinecraftClient;
+
+public class Fullbright extends Module {
+
+    private final SliderSetting brightness = addSetting(new SliderSetting(
+            "Brightness", "Parlaklık seviyesi", 10.0, 1.0, 10.0, 0.5
+    ));
+
+    private double oldGamma = 1.0;
+
+    public Fullbright() {
+        super("Fullbright", "Gamma değerini artırarak karanlığı kaldırır", Category.MISC);
+    }
+
+    @Override
+    public void onEnable() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.options != null) {
+            oldGamma = client.options.getGamma();
+            client.options.setGamma(brightness.getValue());
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.options != null) {
+            client.options.setGamma(oldGamma);
+        }
+    }
+
+    @Override
+    public void onTick(MinecraftClient client) {
+        if (client.options != null && client.options.getGamma() < brightness.getValue()) {
+            client.options.setGamma(brightness.getValue());
+        }
+    }
+}
---
+++ src/main/java /com/hext/client/module/impl /misc/NoHurtCam.java
@@ -0,0 +1,11 @@
+package com.hextclient.client.module.impl.misc;
+
+import com.hextclient.client.module.Module;
+import com.hextclient.client.module.category.Category;
+
+public class NoHurtCam extends Module {
+
+    public NoHurtCam() {
+        super("NoHurtCam", "Hasar alırken kamera sarsıntısını kaldırır", Category.MISC);
+    }
+}
