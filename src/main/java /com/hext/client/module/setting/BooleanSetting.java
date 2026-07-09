fb3bebc7ab9fe71367ee24334b10e00208a338c1
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 01:11:55 +0300

    Add files via upload

---
+++ src/main/java /com/hext/client/module/setting/BooleanSetting.java
@@ -0,0 +1,13 @@
+package com.hextclient.client.module.setting;
+
+public class BooleanSetting extends Setting<Boolean> {
+
+    public BooleanSetting(String name, String description, boolean defaultValue) {
+        super(name, description, defaultValue);
+    }
+
+    public void toggle() { value = !value; }
+
+    @Override
+    public String getDisplayValue() { return value ? "ON" : "OFF"; }
+}
---
+++ src/main/java /com/hext/client/module/setting/ColorSetting.java
@@ -0,0 +1,16 @@
+package com.hextclient.client.module.setting;
+
+public class ColorSetting extends Setting<Integer> {
+
+    public ColorSetting(String name, String description, int defaultColor) {
+        super(name, description, defaultColor);
+    }
+
+    public int getRed() { return (value >> 16) & 0xFF; }
+    public int getGreen() { return (value >> 8) & 0xFF; }
+    public int getBlue() { return value & 0xFF; }
+    public int getAlpha() { return (value >> 24) & 0xFF; }
+
+    @Override
+    public String getDisplayValue() { return String.format("#%08X", value); }
+}
