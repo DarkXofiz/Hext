0fb9063843c4883b698064b8cedb88d909698bdb
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 03:02:15 +0300

    Fix package and import statements in Hitbox.java
    Corrected package name and import statements for Hitbox class.

--- src/main/java /com/hext/client/module/impl /combat/Hitbox.java
+++ src/main/java /com/hext/client/module/impl /combat/Hitbox.java
@@ -1,8 +1,8 @@
-package com.hextclient.client.module.impl.combat;
+package com.hext.client.module.impl.combat;
 
-import com.hextclient.client.module.Module;
-import com.hextclient.client.module.category.Category;
-import com.hextclient.client.module.setting.SliderSetting;
+import com.hext.client.module.Module;
+import com.hext.client.module.category.Category;
+import com.hext.client.module.setting.SliderSetting;
 import net.minecraft.client.MinecraftClient;
 
 public class Hitbox extends Module {
