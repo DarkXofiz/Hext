02c4fa0e2f4143deca29c21e2511301320da336c
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 18:57:52 +0300

    Update Reach.java

--- src/main/java/com/hext/client/module/impl/combat/Reach.java
+++ src/main/java/com/hext/client/module/impl/combat/Reach.java
@@ -2,23 +2,17 @@ package com.hext.client.module.impl.combat;
 
 import com.hext.client.module.Module;
 import com.hext.client.module.category.Category;
-import com.hext.client.module.setting.BooleanSetting;
 import com.hext.client.module.setting.SliderSetting;
 
 public class Reach extends Module {
 
     private final SliderSetting distance = addSetting(new SliderSetting(
-            "Distance", "Erişim mesafesi (blok)", 3.0, 3.0, 6.0, 0.1
-    ));
-
-    private final BooleanSetting showIndicator = addSetting(new BooleanSetting(
-            "Show Indicator", "HUD'da mesafeyi göster", true
+            "Distance", "Saldırı menzili", 3.5, 3.0, 7.0, 0.1
     ));
 
     public Reach() {
-        super("Reach", "Saldırı menzilini artırır", Category.COMBAT);
+        super("Reach", "Saldırı menzilini gösterir", Category.COMBAT);
     }
 
     public double getDistance() { return distance.getValue(); }
-    public boolean shouldShowIndicator() { return showIndicator.getValue(); }
 }
