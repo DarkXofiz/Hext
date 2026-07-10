bc36c0eeec3831c9e240faef70f9d186b731fe28
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 04:03:20 +0300

    Add files via upload

---
+++ src/main/java /com/hext/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,19 @@
+package com.hext.mixin;
+
+import com.hext.client.Hext;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+
+    @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
+    private void onSendChatMessage(String message, net.minecraft.network.message.LastSeenMessageList.Acknowledgment acknowledgment, CallbackInfo ci) {
+        if (Hext.getInstance().getCommandManager().handleMessage(message)) {
+            ci.cancel();
+        }
+    }
+}
