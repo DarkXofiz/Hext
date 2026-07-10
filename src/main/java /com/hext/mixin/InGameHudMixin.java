943f1f082755c86d23fbed374a7a8b1bf15c87ca
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 04:52:31 +0300

    Delete src/main/java /com directory

--- src/main/java /com/hext/mixin/InGameHudMixin.java
+++
@@ -1,9 +0,0 @@
-package com.hext.mixin;
-
-import net.minecraft.client.gui.hud.InGameHud;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(InGameHud.class)
-public class InGameHudMixin {
-    // Reserved for future HUD overrides (e.g., custom crosshair, hotbar styling)
-}
--- src/main/java /com/hext/mixin/WorldRendererMixin.java
+++
@@ -1,9 +0,0 @@
-package com.hext.mixin;
-
-import net.minecraft.client.render.WorldRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(WorldRenderer.class)
-public class WorldRendererMixin {
-    // Reserved for world-level render hooks (e.g., custom sky color, ESP outlines)
-}
