01f248acdda9a1e0ef534e73aef34de534351cd6
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 03:07:32 +0300

    Fix package path in ToggleSprint.java

--- src/main/java /com/hext/client/module/impl /movement/ToggleSprint.java
+++ src/main/java /com/hext/client/module/impl /movement/ToggleSprint.java
@@ -1,8 +1,8 @@
-package com.hextclient.client.module.impl.movement;
+package com.hext.client.module.impl.movement;
 
-import com.hextclient.client.module.Module;
-import com.hextclient.client.module.category.Category;
-import com.hextclient.client.module.setting.BooleanSetting;
+import com.hext.client.module.Module;
+import com.hext.client.module.category.Category;
+import com.hext.client.module.setting.BooleanSetting;
 import net.minecraft.client.MinecraftClient;
 
 public class ToggleSprint extends Module {
