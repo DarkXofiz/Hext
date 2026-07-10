2e6eff813be957ccc7960596a92a5fb2481da9a3
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 04:00:05 +0300

    Add files via upload

---
+++ src/main/java /com/hext/client/command/impl/ConfigCommand.java
@@ -0,0 +1,31 @@
+package com.hext.client.command.impl;
+
+import com.hext.client.Hext;
+import com.hext.client.command.Command;
+import com.hext.client.command.CommandManager;
+
+public class ConfigCommand extends Command {
+
+    public ConfigCommand() {
+        super("config", "Konfigürasyonu kaydet/yükle. Kullanım: .config <save|load>", "cfg");
+    }
+
+    @Override
+    public void execute(String[] args) {
+        if (args.length == 0) {
+            CommandManager.sendMessage("\u00A7cKullanım: .config <save|load>");
+            return;
+        }
+        switch (args[0].toLowerCase()) {
+            case "save" -> {
+                Hext.getInstance().getConfigManager().save();
+                CommandManager.sendMessage("\u00A7aKonfig kaydedildi.");
+            }
+            case "load" -> {
+                Hext.getInstance().getConfigManager().load();
+                CommandManager.sendMessage("\u00A7aKonfig yüklendi.");
+            }
+            default -> CommandManager.sendMessage("\u00A7cGeçersiz eylem: " + args[0]);
+        }
+    }
+}
