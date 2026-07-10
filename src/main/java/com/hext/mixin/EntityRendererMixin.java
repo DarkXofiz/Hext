735c436e1ea1a47ddee3556fb31e4b0c48e1fd65
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 12:42:04 +0300

    Add files via upload

---
+++ src/main/java/com/hext/mixin/EntityRendererMixin.java
@@ -0,0 +1,24 @@
+package com.hext.mixin;
+
+import com.hext.client.Hext;
+import com.hext.client.module.impl.combat.Hitbox;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Box;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(Entity.class)
+public class EntityRendererMixin {
+
+    @ModifyVariable(method = "getVisibilityBoundingBox", at = @At("RETURN"), ordinal = 0)
+    private Box expandHitbox(Box box) {
+        Hitbox mod = Hext.getInstance().getModuleManager()
+                .getModule(Hitbox.class).orElse(null);
+        if (mod == null || !mod.isEnabled()) return box;
+        Entity self = (Entity)(Object)this;
+        net.minecraft.client.MinecraftClient mc = net.minecraft.client.MinecraftClient.getInstance();
+        if (mc.player != null && mc.player.equals(self)) return box;
+        return box.expand(mod.getSize(), mod.getHeight(), mod.getSize());
+    }
+}
