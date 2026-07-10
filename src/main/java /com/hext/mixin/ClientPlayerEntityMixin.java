700e5f992cf7f9c7fc5aa474e44f0ea29099f1ec
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-10 03:16:48 +0300

    Fix package name and update HextClient reference

--- src/main/java /com/hext/mixin/ClientPlayerEntityMixin.java
+++ src/main/java /com/hext/mixin/ClientPlayerEntityMixin.java
@@ -1,6 +1,6 @@
-package com.hextclient.mixin;
+package com.hext.mixin;
 
-import com.hextclient.client.HextClient;
+import com.hext.client.HextClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +12,7 @@ public class ClientPlayerEntityMixin {
 
     @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
     private void onSendChatMessage(String message, net.minecraft.network.message.LastSeenMessageList.Acknowledgment acknowledgment, CallbackInfo ci) {
-        if (HextClient.getInstance().getCommandManager().handleMessage(message)) {
+        if (Hext.getInstance().getCommandManager().handleMessage(message)) {
             ci.cancel();
         }
     }
