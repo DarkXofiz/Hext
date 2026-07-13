b8fdd6f44668b924fe98a9d1c9949aef65d0744c
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-13 07:52:31 +0300

    Refactor ElytraSwap logic for item swapping

--- src/main/java/com/hext/modules/ElytraSwap.java
+++ src/main/java/com/hext/modules/ElytraSwap.java
@@ -17,10 +17,9 @@ public class ElytraSwap extends BaseModule {
         ItemStack chest = mc.player.getInventory().getArmorStack(EquipmentSlot.CHEST.getEntitySlotId());
         if (chest.getItem() == Items.ELYTRA) {
             if (state) {
-                // Swap to hotbar slot 0 (chestplate)
                 for (int i = 0; i < 9; i++) {
-                    if (mc.player.getInventory().getStack(i).getItem() == Items.DIAMOND_CHESTPLATE ||
-                        mc.player.getInventory().getStack(i).getItem() == Items.NETHERITE_CHESTPLATE) {
+                    ItemStack stack = mc.player.getInventory().getStack(i);
+                    if (stack.getItem() == Items.DIAMOND_CHESTPLATE || stack.getItem() == Items.NETHERITE_CHESTPLATE) {
                         mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, i, 0, SlotActionType.PICKUP, mc.player);
                         mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, 6, 0, SlotActionType.PICKUP, mc.player);
                         state = false;
