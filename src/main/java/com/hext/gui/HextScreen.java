b85a7dd971d81b3dc0d90931c7a42a65b3708fd7
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-13 14:35:45 +0300

    Update HextScreen.java

--- src/main/java/com/hext/gui/HextScreen.java
+++ src/main/java/com/hext/gui/HextScreen.java
@@ -1,24 +1,21 @@
 package com.hext.gui;
 
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
 public class HextScreen extends Screen {
     private static final int SETTING_H = 20;
     private static final int MARGIN = 10;
-    
-    // Buraya yalnızca HextScreen class'ının kodu gelecek
-    // BaseModule class'ı BURADA OLMAMALI!
 
     public HextScreen() {
         super(Text.literal("Hext Client"));
     }
 
     @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        this.renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
     }
 
     @Override
