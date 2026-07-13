454ed83461b6c56cf2ec93b8b06b2f3a69723617
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-13 07:35:22 +0300

    Update HextClient.java

--- src/main/java/com/hext/HextClient.java
+++ src/main/java/com/hext/HextClient.java
@@ -1,18 +1,26 @@
 package com.hext;
 
+import com.hext.config.Config;
+import com.hext.modules.BaseModule;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HextClient implements ModInitializer, ClientModInitializer {
     public static final String MOD_ID = "hext";
+    public static MinecraftClient mc;
+    public static List<BaseModule> modules = new ArrayList<>();
 
     @Override
-    public void onInitialize() {
-        // Fabric API init
-    }
+    public void onInitialize() {}
 
     @Override
     public void onInitializeClient() {
+        mc = MinecraftClient.getInstance();
+        Config.load();
         ModuleManager.getInstance().init();
     }
 }
