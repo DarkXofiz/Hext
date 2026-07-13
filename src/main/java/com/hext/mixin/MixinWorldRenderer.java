0cf9b32bfaede4f4bd9c8a780dbc623f496b697f
DarkXofiz <ahmeetmuhammet78@gmail.com>
2026-07-13 07:37:47 +0300

    Rename MiniWorldRenderer to MixinWorldRenderer

--- src/main/java/com/hext/mixin/MixinWorldRenderer.java
+++ src/main/java/com/hext/mixin/MixinWorldRenderer.java
@@ -1,11 +1,9 @@
 package com.hext.mixin;
 
-import com.hext.HextClient;
-import com.hext.modules.ESP;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer {
-    // Hook used by WorldRenderEvents, no need to inject here as we use Fabric API events.
+    // Fabric API events kullanıldığı için boş
 }
